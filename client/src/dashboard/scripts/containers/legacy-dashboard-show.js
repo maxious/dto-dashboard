9239fff3efdd8072cee9d87aac1c4c6077f1716f
elisechant <elise.chant@gmail.com>
2016-09-08 13:02:44 +1000

    move lgeacy data serialize outside;

--- client/src/dashboard/scripts/containers/legacy-dashboard-show.js
+++ client/src/dashboard/scripts/containers/legacy-dashboard-show.js
@@ -1,7 +1,4 @@
-import { select, selectAll } from 'd3-selection';
-import { createWidgets } from './../actions/widgets';
-import { createDatasets } from './../actions/datasets';
-import { createDatapoints } from './../actions/datapoints';
+import { select } from 'd3-selection';
 
 import FactWidget from './../legacy-components/Widgets/FactWidget';
 
@@ -15,48 +12,9 @@ class LegacyDashboardShow {
         self.props = newState;
         self.render();
       }
-      // if (newState !== oldState) {
       // no requirement to handle this at this stage
-      // }
+      // if (newState !== oldState) {}
     });
-    this.serializeWidgets(this.fetchWidgets(), store.dispatch);
-  }
-
-  serializeWidgets(widgets, dispatch) {
-    widgets.forEach((w) => {
-      w.datasets.forEach((d) => {
-        d.widget_id = w.id;
-      });
-      dispatch(createDatasets(w.datasets));
-      delete w.datasets;
-    });
-    dispatch(createWidgets(widgets));
-  }
-
-  // todo - move this to main.js
-  fetchWidgets() {
-    let _widgetsData = [];
-    selectAll('.bar .widget__inner').each(function() {
-      _widgetsData.push(JSON.parse(this.getAttribute('data-data')));
-    });
-    selectAll('.sparkline .widget__inner').each(function() {
-      _widgetsData.push(JSON.parse(this.getAttribute('data-data')));
-    });
-    selectAll('.line .widget__inner').each(function() {
-      _widgetsData.push(JSON.parse(this.getAttribute('data-data')));
-    });
-    selectAll('.pie .widget__inner').each(function() {
-      _widgetsData.push(JSON.parse(this.getAttribute('data-data')));
-    });
-    selectAll('.fact .widget__inner').each(function() {
-      _widgetsData.push(JSON.parse(this.getAttribute('data-data')));
-    });
-    return _widgetsData;
-  }
-
-  onPropsChange() {
-    console.log(this.props);
-    // todo
   }
 
   render() {
@@ -64,65 +22,32 @@ class LegacyDashboardShow {
       let el = select(`[data-id="${w.id}"] .widget__inner`);
 
       switch (w.type) {
-        // case 'bar':
-        //   break;
+        case 'bar':
+          break;
+
         case 'fact':
           new FactWidget({
             description: w.description,
             element: el
           });
           break;
-        // case 'pie':
+
+        case 'pie':
+          break;
+
+        // case 'kpi-sparkline':
         //   break;
+        //
+        // case 'sparkline':
+        //   break;
+
+        default:
+          console.error('No chart type for widget type:', w.type);
       }
     });
+    return this;
   }
 
 }
 
 export default LegacyDashboardShow;
-
-//
-// let data = {
-//   "id": 22,
-//   "name": "Devices used by users",
-//   "type": "bar",
-//   "size": "medium",
-//   "latest": {"value": "50.0", "label": "2016-07"},
-//   "units": "%",
-//   "definition": "This shows the types of devices used by users to access the appointment booking service.",
-//   "description": "This shows the types of devices used by users to access the appointment booking service.",
-//   "updated_at": "2016-09-06T06:09:17.793Z",
-//   "prefix": "",
-//   "suffix": "%",
-//   "stacking": "percentage",
-//   "displayRoundedData": true,
-//   "datasets": [{
-//     "id": "mobile",
-//     "name": "Mobile",
-//     "units": "i",
-//     "notes": "note for this dataset",
-//     "data": [{"value": "50.0", "label": "2016-07"}, {"value": "49.0", "label": "2016-06"}, {
-//       "value": "47.0",
-//       "label": "2016-05"
-//     }, {"value": "48.0", "label": "2016-04"}, {"value": "43.0", "label": "2016-03"}]
-//   }, {
-//     "id": "tablet",
-//     "name": "Tablet",
-//     "units": "i",
-//     "notes": "note for this dataset",
-//     "data": [{"value": "3.0", "label": "2016-07"}, {"value": "2.0", "label": "2016-06"}, {
-//       "value": "6.0",
-//       "label": "2016-05"
-//     }, {"value": "6.0", "label": "2016-04"}, {"value": "6.0", "label": "2016-03"}]
-//   }, {
-//     "id": "desktop",
-//     "name": "Desktop",
-//     "units": "i",
-//     "notes": "note for this dataset",
-//     "data": [{"value": "47.0", "label": "2016-07"}, {"value": "49.0", "label": "2016-06"}, {
-//       "value": "47.0",
-//       "label": "2016-05"
-//     }, {"value": "46.0", "label": "2016-04"}, {"value": "51.0", "label": "2016-03"}]
-//   }]
-// }
--- client/src/dashboard/scripts/main.js
+++ client/src/dashboard/scripts/main.js
@@ -1,8 +1,12 @@
 import { merge } from 'lodash';
+import domready from 'domready';
+import { select, selectAll } from 'd3-selection';
 
 import configureStore from './store/configureStore';
 import initialState from './store/initialState';
-
+import { createWidgets } from './../actions/widgets';
+import { createDatasets } from './../actions/datasets';
+import { createDatapoints } from './../actions/datapoints';
 import DashboardShow from './containers/legacy-dashboard-show';
 
 
@@ -11,9 +15,40 @@ const bootState = merge(initialState, window.__INITIAL_STATE__);
 const store = configureStore(bootState, true);
 
 
+domready(() => {
+
+  let _widgetsData = [];
+  selectAll('.bar .widget__inner').each(function() {
+    _widgetsData.push(JSON.parse(this.getAttribute('data-data')));
+  });
+  selectAll('.sparkline .widget__inner').each(function() {
+    _widgetsData.push(JSON.parse(this.getAttribute('data-data')));
+  });
+  selectAll('.line .widget__inner').each(function() {
+    _widgetsData.push(JSON.parse(this.getAttribute('data-data')));
+  });
+  selectAll('.pie .widget__inner').each(function() {
+    _widgetsData.push(JSON.parse(this.getAttribute('data-data')));
+  });
+  selectAll('.fact .widget__inner').each(function() {
+    _widgetsData.push(JSON.parse(this.getAttribute('data-data')));
+  });
+
+  // serialize
+  _widgetsData.forEach((w) => {
+    w.datasets.forEach((d) => {
+      d.widget_id = w.id;
+    });
+    store.dispatch(createDatasets(w.datasets));
+    delete w.datasets;
+  });
+  store.dispatch(createWidgets(_widgetsData));
 
 
-let DashboardShowView = new DashboardShow(store);
+  new DashboardShow(store);
+
+});
+
 
 
 
