7b281064d2ba2a1db13bedda8c8c1d9401f9d287
Toby Hede <tobyhede@info-architects.net>
2016-10-05 10:04:01 +1100

    Update new relic deploy script

--- bin/ci-deploy-newrelic.sh
+++ bin/ci-deploy-newrelic.sh
@@ -1,3 +1,12 @@
+echo "{
+  \"deployment\": {
+    \"revision\": \"$CIRCLE_SHA1\",
+    \"changelog\": \"$CIRCLE_BUILD_URL\",
+    \"description\": \"$CIRCLE_BUILD_URL\",
+    \"user\": \"circleci\"
+  }
+}"
+
 curl -X POST 'https://api.newrelic.com/v2/applications/25255438/deployments.json' \
      -H 'X-Api-Key:$NEW_RELIC_API_KEY' -i \
      -H 'Content-Type: application/json' \
